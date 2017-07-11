f4e3333d1bb37fe2941678298ef256a5efb6c0e5
Mr.GZT <Mr.GZT@outlook.com>
2017-07-11 17:30:07 +0800

    Implementing Mechanical Arm

---
+++ MechanicalArm.cpp
@@ -0,0 +1,42 @@
+#include <MechanicalArm.h>
+#include <Arduino.h>
+
+MechanicalArm::MechanicalArm(int vPin,
+                             int hPinL,
+                             int hPinR) : vertical(vPin),
+                                          horizontalL(hPinL),
+                                          horizontalL(hPinR)
+{
+}
+
+void MechanicalArm::init()
+{
+    svrV.detach();
+    svrHL.detach();
+    svrHR.detach();
+    svrV.attach(vertical);
+    svrHL.attach(horizontalL);
+    svrHR.attach(horizontalR);
+}
+
+void MechanicalArm::putUp()
+{
+    svrV.write(90);
+}
+
+void MechanicalArm::putDown()
+{
+    svrV.write(0);
+}
+
+void MechanicalArm::grab()
+{
+    svrHL.write(horizontalDegree);
+    svrHR.write(-horizontalDegree);
+}
+
+void MechanicalArm::release()
+{
+    svrHL.write(0);
+    svrHR.write(0);
+}
---
+++ MechanicalArm.h
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+class MechanicalArm
+{
+  private:
+    const int horizontalDegree = 45;
+  public:
+    int vertical = 0, horizontalL = 0, horizontalR = 0;
+    Servo svrV, svrHL, svrHR;
+    MechanicalArm(int);
+    ~MechanicalArm();
+
+    void init();
+
+    void putUp();
+    void putDown();
+    void grab();
+    void release();
+}
